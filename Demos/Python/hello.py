f042b63fd466a178739b6551c0da266a8ac71f8e
billKarsh <karshb@janelia.hhmi.org>
2023-07-18 17:48:57 -0700

    Stronger Python path

--- Demos/Python/hello.py
+++ Demos/Python/hello.py
@@ -1,6 +1,6 @@
 import subprocess
 
-HelloSGLX = "C:/Users/labadmin/Desktop/HelloSGLX-win/HelloSGLX.exe"
+HelloSGLX = r"C:/Users/labadmin/Desktop/HelloSGLX-win/HelloSGLX.exe"
 args = "-host=127.0.0.1 -cmd=getVersion"
 
 ret = subprocess.run(HelloSGLX+" "+args,capture_output=True,shell=True)
